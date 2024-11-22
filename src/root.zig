4c2fb3a96811d337dae902d9b5122550bf7d3639
unorsk <25188+unorsk@users.noreply.github.com>
2024-11-22 09:16:17 +0100

    saving

--- src/root.zig
+++ src/root.zig
@@ -48,6 +48,7 @@ const Cvss31MetricType = enum {
 
 const Cvss31MetricDecl = struct {
     metricType: Cvss31MetricType,
+    metricTypeValue: []const u8,
     required: bool,
     possibleValues: []const []const u8,
 };
@@ -62,29 +63,68 @@ const CvssParseError = error{
 };
 
 const Cvss31Decl: []const Cvss31MetricDecl = &.{
-    .{ .metricType = Cvss31MetricType.AV, .required = true, .possibleValues = &.{ "N", "A", "L", "P" } },
-    .{ .metricType = Cvss31MetricType.AC, .required = true, .possibleValues = &.{ "L", "H" } },
-    .{ .metricType = Cvss31MetricType.PR, .required = true, .possibleValues = &.{ "N", "L", "H" } },
-    .{ .metricType = Cvss31MetricType.UI, .required = true, .possibleValues = &.{ "N", "R" } },
-    .{ .metricType = Cvss31MetricType.S, .required = true, .possibleValues = &.{ "U", "C" } },
-    .{ .metricType = Cvss31MetricType.C, .required = true, .possibleValues = &.{ "N", "L", "H" } },
-    .{ .metricType = Cvss31MetricType.I, .required = true, .possibleValues = &.{ "N", "L", "H" } },
-    .{ .metricType = Cvss31MetricType.A, .required = true, .possibleValues = &.{ "N", "L", "H" } },
-    .{ .metricType = Cvss31MetricType.E, .required = false, .possibleValues = &.{ "X", "U", "P", "F", "H" } },
-    .{ .metricType = Cvss31MetricType.RL, .required = false, .possibleValues = &.{ "X", "O", "T", "W", "U", "P", "C", "H" } },
-    .{ .metricType = Cvss31MetricType.RC, .required = false, .possibleValues = &.{ "X", "U", "R", "C", "H" } },
-    .{ .metricType = Cvss31MetricType.CR, .required = false, .possibleValues = &.{ "X", "L", "M", "H" } },
-    .{ .metricType = Cvss31MetricType.IR, .required = false, .possibleValues = &.{ "X", "L", "M", "H" } },
-    .{ .metricType = Cvss31MetricType.AR, .required = false, .possibleValues = &.{ "X", "L", "" } },
-    .{ .metricType = Cvss31MetricType.MAV, .required = false, .possibleValues = &.{ "X", "N", "A", "L", "P" } },
-    .{ .metricType = Cvss31MetricType.MAC, .required = false, .possibleValues = &.{ "X", "H", "L" } },
-    .{ .metricType = Cvss31MetricType.MPR, .required = false, .possibleValues = &.{ "X", "N", "L", "H" } },
-    .{ .metricType = Cvss31MetricType.MUI, .required = false, .possibleValues = &.{ "X", "N", "R" } },
-    .{ .metricType = Cvss31MetricType.MS, .required = false, .possibleValues = &.{ "X", "U", "C" } },
-    .{ .metricType = Cvss31MetricType.MC, .required = false, .possibleValues = &.{ "X", "N", "L", "H" } },
-    .{ .metricType = Cvss31MetricType.MI, .required = false, .possibleValues = &.{ "X", "N", "L", "H" } },
-    .{ .metricType = Cvss31MetricType.MA, .required = false, .possibleValues = &.{ "X", "N", "L", "H" } },
+    .{ .metricType = Cvss31MetricType.AV, .metricTypeValue = "AV", .required = true, .possibleValues = &.{ "N", "A", "L", "P" } },
+    .{ .metricType = Cvss31MetricType.AC, .metricTypeValue = "AC", .required = true, .possibleValues = &.{ "L", "H" } },
+    .{ .metricType = Cvss31MetricType.PR, .metricTypeValue = "PR", .required = true, .possibleValues = &.{ "N", "L", "H" } },
+    .{ .metricType = Cvss31MetricType.UI, .metricTypeValue = "UI", .required = true, .possibleValues = &.{ "N", "R" } },
+    .{ .metricType = Cvss31MetricType.S, .metricTypeValue = "S", .required = true, .possibleValues = &.{ "U", "C" } },
+    .{ .metricType = Cvss31MetricType.C, .metricTypeValue = "C", .required = true, .possibleValues = &.{ "N", "L", "H" } },
+    .{ .metricType = Cvss31MetricType.I, .metricTypeValue = "I", .required = true, .possibleValues = &.{ "N", "L", "H" } },
+    .{ .metricType = Cvss31MetricType.A, .metricTypeValue = "A", .required = true, .possibleValues = &.{ "N", "L", "H" } },
+    .{ .metricType = Cvss31MetricType.E, .metricTypeValue = "E", .required = false, .possibleValues = &.{ "X", "U", "P", "F", "H" } },
+    .{ .metricType = Cvss31MetricType.RL, .metricTypeValue = "RL", .required = false, .possibleValues = &.{ "X", "O", "T", "W", "U", "P", "C", "H" } },
+    .{ .metricType = Cvss31MetricType.RC, .metricTypeValue = "RC", .required = false, .possibleValues = &.{ "X", "U", "R", "C", "H" } },
+    .{ .metricType = Cvss31MetricType.CR, .metricTypeValue = "CR", .required = false, .possibleValues = &.{ "X", "L", "M", "H" } },
+    .{ .metricType = Cvss31MetricType.IR, .metricTypeValue = "IR", .required = false, .possibleValues = &.{ "X", "L", "M", "H" } },
+    .{ .metricType = Cvss31MetricType.AR, .metricTypeValue = "AR", .required = false, .possibleValues = &.{ "X", "L", "" } },
+    .{ .metricType = Cvss31MetricType.MAV, .metricTypeValue = "MAV", .required = false, .possibleValues = &.{ "X", "N", "A", "L", "P" } },
+    .{ .metricType = Cvss31MetricType.MAC, .metricTypeValue = "MAC", .required = false, .possibleValues = &.{ "X", "H", "L" } },
+    .{ .metricType = Cvss31MetricType.MPR, .metricTypeValue = "MPR", .required = false, .possibleValues = &.{ "X", "N", "L", "H" } },
+    .{ .metricType = Cvss31MetricType.MUI, .metricTypeValue = "MUI", .required = false, .possibleValues = &.{ "X", "N", "R" } },
+    .{ .metricType = Cvss31MetricType.MS, .metricTypeValue = "MS", .required = false, .possibleValues = &.{ "X", "U", "C" } },
+    .{ .metricType = Cvss31MetricType.MC, .metricTypeValue = "MC", .required = false, .possibleValues = &.{ "X", "N", "L", "H" } },
+    .{ .metricType = Cvss31MetricType.MI, .metricTypeValue = "MI", .required = false, .possibleValues = &.{ "X", "N", "L", "H" } },
+    .{ .metricType = Cvss31MetricType.MA, .metricTypeValue = "MA", .required = false, .possibleValues = &.{ "X", "N", "L", "H" } },
 };
+
+fn parse_cvss31_metrics(cvss: []const u8) ![]Cvss31Metric {
+    // fn parse_cvss31_metrics(cvss: []const u8) !void {
+    var metrics = std.ArrayList(Cvss31Metric).init(std.heap.page_allocator);
+    // const metricsA = [_]Cvss31Metric{};
+
+    var i: usize = 0;
+    while (i < cvss.len) {
+        // var metricType: Cvss31MetricType = undefined;
+
+        for (Cvss31Decl) |decl| {
+            if (std.mem.eql(u8, cvss[i..], decl.metricTypeValue)) {
+                i += decl.metricTypeValue.len;
+                i += 1; // skip the colon
+
+                var value: []const u8 = undefined;
+                for (0..(decl.possibleValues.len - 1)) |j| {
+                    value = decl.possibleValues[j];
+                    if (std.mem.eql(u8, cvss[i..], decl.possibleValues[j])) {
+                        try metrics.append(.{ .metricType = decl.metricType, .value = value });
+                        break;
+                    }
+                }
+                // if (value == undefined) {
+                //     return CvssParseError.NotCVSSString;
+                // }
+            }
+        }
+
+        // if (metricType == undefined) {
+        //     return CvssParseError.NotCVSSString;
+        // }
+
+        i += 1;
+        // i += value.len;
+    }
+
+    return metrics.items;
+}
 
 fn detect_cvss_version(cvss: []const u8) CvssParseError!CVSS_VERSION {
     if (std.mem.eql(u8, cvss, CVSS20_HEADER)) {
@@ -102,6 +142,18 @@ fn detect_cvss_version(cvss: []const u8) CvssParseError!CVSS_VERSION {
 
 pub export fn add(a: i32, b: i32) i32 {
     return a + b;
+}
+
+test "parse simple AV metric" {
+    const cvss = "AV:N";
+    const metrics = try parse_cvss31_metrics(cvss);
+    std.debug.print("size: {}\n", .{metrics.len});
+    // std.debug.print("val {}\n", .{metrics[0].metricType});
+    // std.debug.print("val {}\n", .{metrics[1].metricType});
+    // std.debug.print("{}\n", .{metrics});
+    try testing.expect(metrics.len == 1);
+    try testing.expect(metrics[0].metricType == Cvss31MetricType.AV);
+    try testing.expect(std.mem.eql(u8, metrics[0].value, "N"));
 }
 
 test "detect_cvss_version" {
