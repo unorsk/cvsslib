c72048c06d48b0a448165c697cb5738d0a2598e0
unorsk <25188+unorsk@users.noreply.github.com>
2025-01-01 12:29:26 +0100

    started work on the scoring for cvss31

--- src/cvss31.zig
+++ src/cvss31.zig
@@ -32,38 +32,41 @@ pub const Cvss31MetricDecl = struct {
     metricType: Cvss31MetricType,
     metricTypeValue: []const u8,
     required: bool,
+    weights: []const f16,
     possibleValues: []const []const u8,
 };
 
 pub const Cvss31Metric = struct {
     metricType: Cvss31MetricType,
     value: []const u8,
+    weight: f16,
 };
 
 // zig fmt: off
 pub const Cvss31Decl: []const Cvss31MetricDecl = &.{
-    .{ .isRead = false, .metricType = Cvss31MetricType.AV,  .metricTypeValue = "AV",  .required = true,  .possibleValues = &.{ "N", "A", "L", "P" } },
-    .{ .isRead = false, .metricType = Cvss31MetricType.AC,  .metricTypeValue = "AC",  .required = true,  .possibleValues = &.{ "L", "H" } },
-    .{ .isRead = false, .metricType = Cvss31MetricType.PR,  .metricTypeValue = "PR",  .required = true,  .possibleValues = &.{ "N", "L", "H" } },
-    .{ .isRead = false, .metricType = Cvss31MetricType.UI,  .metricTypeValue = "UI",  .required = true,  .possibleValues = &.{ "N", "R" } },
-    .{ .isRead = false, .metricType = Cvss31MetricType.S,   .metricTypeValue = "S",   .required = true,  .possibleValues = &.{ "U", "C" } },
-    .{ .isRead = false, .metricType = Cvss31MetricType.C,   .metricTypeValue = "C",   .required = true,  .possibleValues = &.{ "N", "L", "H" } },
-    .{ .isRead = false, .metricType = Cvss31MetricType.I,   .metricTypeValue = "I",   .required = true,  .possibleValues = &.{ "N", "L", "H" } },
-    .{ .isRead = false, .metricType = Cvss31MetricType.A,   .metricTypeValue = "A",   .required = true,  .possibleValues = &.{ "N", "L", "H" } },
-    .{ .isRead = false, .metricType = Cvss31MetricType.E,   .metricTypeValue = "E",   .required = false, .possibleValues = &.{ "X", "U", "P", "F", "H" } },
-    .{ .isRead = false, .metricType = Cvss31MetricType.RL,  .metricTypeValue = "RL",  .required = false, .possibleValues = &.{ "X", "O", "T", "W", "U", "P", "C", "H" } },
-    .{ .isRead = false, .metricType = Cvss31MetricType.RC,  .metricTypeValue = "RC",  .required = false, .possibleValues = &.{ "X", "U", "R", "C", "H" } },
-    .{ .isRead = false, .metricType = Cvss31MetricType.CR,  .metricTypeValue = "CR",  .required = false, .possibleValues = &.{ "X", "L", "M", "H" } },
-    .{ .isRead = false, .metricType = Cvss31MetricType.IR,  .metricTypeValue = "IR",  .required = false, .possibleValues = &.{ "X", "L", "M", "H" } },
-    .{ .isRead = false, .metricType = Cvss31MetricType.AR,  .metricTypeValue = "AR",  .required = false, .possibleValues = &.{ "X", "L", "" } },
-    .{ .isRead = false, .metricType = Cvss31MetricType.MAV, .metricTypeValue = "MAV", .required = false, .possibleValues = &.{ "X", "N", "A", "L", "P" } },
-    .{ .isRead = false, .metricType = Cvss31MetricType.MAC, .metricTypeValue = "MAC", .required = false, .possibleValues = &.{ "X", "H", "L" } },
-    .{ .isRead = false, .metricType = Cvss31MetricType.MPR, .metricTypeValue = "MPR", .required = false, .possibleValues = &.{ "X", "N", "L", "H" } },
-    .{ .isRead = false, .metricType = Cvss31MetricType.MUI, .metricTypeValue = "MUI", .required = false, .possibleValues = &.{ "X", "N", "R" } },
-    .{ .isRead = false, .metricType = Cvss31MetricType.MS,  .metricTypeValue = "MS",  .required = false, .possibleValues = &.{ "X", "U", "C" } },
-    .{ .isRead = false, .metricType = Cvss31MetricType.MC,  .metricTypeValue = "MC",  .required = false, .possibleValues = &.{ "X", "N", "L", "H" } },
-    .{ .isRead = false, .metricType = Cvss31MetricType.MI,  .metricTypeValue = "MI",  .required = false, .possibleValues = &.{ "X", "N", "L", "H" } },
-    .{ .isRead = false, .metricType = Cvss31MetricType.MA,  .metricTypeValue = "MA",  .required = false, .possibleValues = &.{ "X", "N", "L", "H" } },
+    .{ .isRead = false, .metricType = Cvss31MetricType.AV,  .metricTypeValue = "AV",  .required = true,  .weights = &.{0.85, 0.62, 0.55, 0.2},  .possibleValues = &.{ "N", "A", "L", "P" } },
+    .{ .isRead = false, .metricType = Cvss31MetricType.AC,  .metricTypeValue = "AC",  .required = true,  .weights = &.{0.44, 0.77},             .possibleValues = &.{ "L", "H" } },
+    .{ .isRead = false, .metricType = Cvss31MetricType.PR,  .metricTypeValue = "PR",  .required = true,  .weights = &.{0, 0, 0},                .possibleValues = &.{ "N", "L", "H" } },// TODO &.{0, 0, 0}
+    .{ .isRead = false, .metricType = Cvss31MetricType.UI,  .metricTypeValue = "UI",  .required = true,  .weights = &.{0.85, 0.62},             .possibleValues = &.{ "N", "R" } },
+    .{ .isRead = false, .metricType = Cvss31MetricType.S,   .metricTypeValue = "S",   .required = true,  .weights = &.{6.42, 7.52},             .possibleValues = &.{ "U", "C" } },
+    .{ .isRead = false, .metricType = Cvss31MetricType.C,   .metricTypeValue = "C",   .required = true,  .weights = &.{0, 0.22, 0.56},          .possibleValues = &.{ "N", "L", "H" } },
+    .{ .isRead = false, .metricType = Cvss31MetricType.I,   .metricTypeValue = "I",   .required = true,  .weights = &.{0, 0.22, 0.56},          .possibleValues = &.{ "N", "L", "H" } },
+    .{ .isRead = false, .metricType = Cvss31MetricType.A,   .metricTypeValue = "A",   .required = true,  .weights = &.{0, 0.22, 0.56},          .possibleValues = &.{ "N", "L", "H" } },
+    .{ .isRead = false, .metricType = Cvss31MetricType.E,   .metricTypeValue = "E",   .required = false, .weights = &.{1, 0.91, 0.94, 0.97, 1}, .possibleValues = &.{ "X", "U", "P", "F", "H" } },
+    .{ .isRead = false, .metricType = Cvss31MetricType.RL,  .metricTypeValue = "RL",  .required = false, .weights = &.{1, 0.95, 0.96, 0.97, 1}, .possibleValues = &.{ "X", "O", "T", "W", "U", "P", "C", "H" } },
+    .{ .isRead = false, .metricType = Cvss31MetricType.RC,  .metricTypeValue = "RC",  .required = false, .weights = &.{1, 0.92, 0.96, 1},       .possibleValues = &.{ "X", "U", "R", "C", "H" } },
+    .{ .isRead = false, .metricType = Cvss31MetricType.CR,  .metricTypeValue = "CR",  .required = false, .weights = &.{1, 0.5, 1, 1.5},         .possibleValues = &.{ "X", "L", "M", "H" } },
+    .{ .isRead = false, .metricType = Cvss31MetricType.IR,  .metricTypeValue = "IR",  .required = false, .weights = &.{1, 0.5, 1, 1.5},         .possibleValues = &.{ "X", "L", "M", "H" } },
+    .{ .isRead = false, .metricType = Cvss31MetricType.AR,  .metricTypeValue = "AR",  .required = false, .weights = &.{1, 0.5, 1, 1.5},         .possibleValues = &.{ "X", "L", "" } },
+
+    .{ .isRead = false, .metricType = Cvss31MetricType.MAV, .metricTypeValue = "MAV", .required = false, .weights = &.{0, 0.85, 0.62, 0.55, 0.2},   .possibleValues = &.{ "X", "N", "A", "L", "P" } },
+    .{ .isRead = false, .metricType = Cvss31MetricType.MAC, .metricTypeValue = "MAC", .required = false, .weights = &.{0, 0.44, 0.77},              .possibleValues = &.{ "X", "H", "L" } },
+    .{ .isRead = false, .metricType = Cvss31MetricType.MPR, .metricTypeValue = "MPR", .required = false, .weights = &.{0, 0, 0, 0},                 .possibleValues = &.{ "X", "N", "L", "H" } }, // TODO &.{0, 0, 0}
+    .{ .isRead = false, .metricType = Cvss31MetricType.MUI, .metricTypeValue = "MUI", .required = false, .weights = &.{0, 0.85, 0.62},              .possibleValues = &.{ "X", "N", "R" } },
+    .{ .isRead = false, .metricType = Cvss31MetricType.MS,  .metricTypeValue = "MS",  .required = false, .weights = &.{0, 6.42, 7.52},              .possibleValues = &.{ "X", "U", "C" } },
+    .{ .isRead = false, .metricType = Cvss31MetricType.MC,  .metricTypeValue = "MC",  .required = false, .weights = &.{0, 0, 0.22, 0.56},           .possibleValues = &.{ "X", "N", "L", "H" } },
+    .{ .isRead = false, .metricType = Cvss31MetricType.MI,  .metricTypeValue = "MI",  .required = false, .weights = &.{0, 0, 0.22, 0.56},           .possibleValues = &.{ "X", "N", "L", "H" } },
+    .{ .isRead = false, .metricType = Cvss31MetricType.MA,  .metricTypeValue = "MA",  .required = false, .weights = &.{0, 0, 0.22, 0.56},           .possibleValues = &.{ "X", "N", "L", "H" } },
 };
 // zig fmt: on
 
@@ -74,6 +77,62 @@ pub fn score(cvss: []const u8) !types.CVSS {
 
 fn score_cvss31(cvss_metrics: []Cvss31Metric) !types.CVSS {
     std.log.debug("metrics {any}", .{cvss_metrics});
+
+    // TODO scoring
+
+    //     var AV = AttackVector;
+    //     var AC = AttackComplexity;
+    //     var PR = PrivilegesRequired;
+    //     var UI = UserInteraction;
+    //     var S  = Scope;
+    //     var C  = Confidentiality;
+    //     var I  = Integrity;
+    //     var A  = Availability;
+
+    //     var E =   ExploitCodeMaturity || "X";
+    //     var RL =  RemediationLevel    || "X";
+    //     var RC =  ReportConfidence    || "X";
+
+    //     var CR =  ConfidentialityRequirement || "X";
+    //     var IR =  IntegrityRequirement       || "X";
+    //     var AR =  AvailabilityRequirement    || "X";
+    //     var MAV = ModifiedAttackVector       || "X";
+    //     var MAC = ModifiedAttackComplexity   || "X";
+    //     var MPR = ModifiedPrivilegesRequired || "X";
+    //     var MUI = ModifiedUserInteraction    || "X";
+    //     var MS =  ModifiedScope              || "X";
+    //     var MC =  ModifiedConfidentiality    || "X";
+    //     var MI =  ModifiedIntegrity          || "X";
+    //     var MA =  ModifiedAvailability       || "X";
+
+    // // WEIGHTS
+    //     var metricWeightAV  = CVSS31.Weight.AV    [AV];
+    //     var metricWeightAC  = CVSS31.Weight.AC    [AC];
+    //     var metricWeightPR  = CVSS31.Weight.PR    [S][PR];  // PR depends on the value of Scope (S).
+    //     var metricWeightUI  = CVSS31.Weight.UI    [UI];
+    //     var metricWeightS   = CVSS31.Weight.S     [S];
+    //     var metricWeightC   = CVSS31.Weight.CIA   [C];
+    //     var metricWeightI   = CVSS31.Weight.CIA   [I];
+    //     var metricWeightA   = CVSS31.Weight.CIA   [A];
+
+    //     var metricWeightE   = CVSS31.Weight.E     [E];
+    //     var metricWeightRL  = CVSS31.Weight.RL    [RL];
+    //     var metricWeightRC  = CVSS31.Weight.RC    [RC];
+
+    //     // For metrics that are modified versions of Base Score metrics, e.g. Modified Attack Vector, use the value of
+    //     // the Base Score metric if the modified version value is "X" ("Not Defined").
+    //     var metricWeightCR  = CVSS31.Weight.CIAR  [CR];
+    //     var metricWeightIR  = CVSS31.Weight.CIAR  [IR];
+    //     var metricWeightAR  = CVSS31.Weight.CIAR  [AR];
+    //     var metricWeightMAV = CVSS31.Weight.AV    [MAV !== "X" ? MAV : AV];
+    //     var metricWeightMAC = CVSS31.Weight.AC    [MAC !== "X" ? MAC : AC];
+    //     var metricWeightMPR = CVSS31.Weight.PR    [MS  !== "X" ? MS  : S] [MPR !== "X" ? MPR : PR];  // Depends on MS.
+    //     var metricWeightMUI = CVSS31.Weight.UI    [MUI !== "X" ? MUI : UI];
+    //     var metricWeightMS  = CVSS31.Weight.S     [MS  !== "X" ? MS  : S];
+    //     var metricWeightMC  = CVSS31.Weight.CIA   [MC  !== "X" ? MC  : C];
+    //     var metricWeightMI  = CVSS31.Weight.CIA   [MI  !== "X" ? MI  : I];
+    //     var metricWeightMA  = CVSS31.Weight.CIA   [MA  !== "X" ? MA  : A];
+
     return types.CVSS{ .CVSS31 = .{ .score = 0, .level = types.CVSS_LEVEL.NONE } };
 }
 
@@ -106,7 +165,7 @@ fn parse_cvss31_metrics(cvss: []const u8) ![]Cvss31Metric {
                 for (0..(decl.possibleValues.len)) |j| {
                     if (std.mem.eql(u8, metricValueRaw, decl.possibleValues[j])) {
                         metricValue = decl.possibleValues[j];
-                        try metrics.append(.{ .metricType = metricType.?, .value = metricValue.? });
+                        try metrics.append(.{ .metricType = metricType.?, .value = metricValue.?, .weight = decl.weights[j] });
                         break;
                     }
                 }
