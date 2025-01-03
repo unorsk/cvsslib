64fe0a6e82cf1b41dda80187756951d3b470abb4
unorsk <25188+unorsk@users.noreply.github.com>
2025-01-03 21:50:56 +0100

    learning about comptime and trying to make it nice :)

--- src/cvss31.zig
+++ src/cvss31.zig
@@ -27,25 +27,51 @@ const Cvss31MetricType = enum {
     MA,
 };
 
+// zig fmt: off
+const Cvss31MetricTypeValue = union(Cvss31MetricType) {
+    AV:  enum { N, A, L, P },
+    AC:  enum { L, H },
+    PR:  enum { N, L, H },
+    UI:  enum { N, R },
+    S:   enum { U, C },
+    C:   enum { N, L, H },
+    I:   enum { N, L, H },
+    A:   enum { N, L, H },
+    E:   enum { X, U, P, F, H },
+    RL:  enum { X, O, T, W, U, P, C, H },
+    RC:  enum { X, U, R, C, H },
+    CR:  enum { X, L, M, H },
+    IR:  enum { X, L, M, H },
+    AR:  enum { X, L, M, H },
+    MAV: enum { X, N, A, L, P },
+    MAC: enum { X, H, L },
+    MPR: enum { X, N, L, H },
+    MUI: enum { X, N, R },
+    MS:  enum { X, U, C },
+    MC:  enum { X, N, L, H },
+    MI:  enum { X, N, L, H },
+    MA:  enum { X, N, L, H },
+};
+// zig fmt: on
+
 pub const Cvss31MetricDef = struct {
     is_read: bool = false,
     metric_type: Cvss31MetricType,
-    value: []const u8,
     required: bool,
     weights: []const f16,
-    constraints: []const []const u8,
 };
 
 pub const Cvss31Metric = struct {
-    metric_type: Cvss31MetricType,
-    value: []const u8,
-    weight: f16,
+    value: Cvss31MetricTypeValue,
+    test_metric_type: if (@import("builtin").mode == .Debug) Cvss31MetricType else void = undefined, // TODO I no longer need this one, but I could use
+    test_value: if (@import("builtin").mode == .Debug) []const u8 else void = undefined,
+    // weight: f16,
 };
 
 // TODO pass by reference
 fn getMetric(metric: Cvss31MetricType, cvss_metrics: []Cvss31Metric) ?Cvss31Metric {
     for (cvss_metrics) |cvss_metric| {
-        if (metric == cvss_metric.metric_type) {
+        if (metric == cvss_metric.test_metric_type) {
             return cvss_metric;
         }
     }
@@ -58,8 +84,13 @@ fn getMetricWeight(metric: Cvss31MetricType, cvss_metrics: []Cvss31Metric) f16 {
     const metric_value = getMetric(metric, cvss_metrics);
     return switch (metric) {
         Cvss31MetricType.AV => metric_value.?.weight,
-        Cvss31MetricType.AC => 0,
-        Cvss31MetricType.PR => 0,
+        Cvss31MetricType.AC => metric_value.?.weight,
+        Cvss31MetricType.PR => {
+            // if (getMetric(Cvss31MetricType.S, cvss_metrics)) |s| {
+            //     if (s.value == 'S') {}
+            // } else {}
+            return 0;
+        },
         Cvss31MetricType.UI => 0,
         Cvss31MetricType.S => 0,
         Cvss31MetricType.C => 0,
@@ -84,33 +115,53 @@ fn getMetricWeight(metric: Cvss31MetricType, cvss_metrics: []Cvss31Metric) f16 {
 
 // zig fmt: off
 pub const cvss31_definitions: []const Cvss31MetricDef = &.{
-    .{ .metric_type = Cvss31MetricType.AV,  .value = "AV",  .required = true,  .weights = &.{0.85, 0.62, 0.55, 0.2},  .constraints = &.{ "N", "A", "L", "P" } },
-    .{ .metric_type = Cvss31MetricType.AC,  .value = "AC",  .required = true,  .weights = &.{0.44, 0.77},             .constraints = &.{ "L", "H" } },
-    .{ .metric_type = Cvss31MetricType.PR,  .value = "PR",  .required = true,  .weights = &.{0.85, 0.62, 0.27,
-                                                                                             0.85, 0.68, 0.5},        .constraints = &.{ "N", "L", "H" } },// TODO &.{0, 0, 0}
-    .{ .metric_type = Cvss31MetricType.UI,  .value = "UI",  .required = true,  .weights = &.{0.85, 0.62},             .constraints = &.{ "N", "R" } },
-    .{ .metric_type = Cvss31MetricType.S,   .value = "S",   .required = true,  .weights = &.{6.42, 7.52},             .constraints = &.{ "U", "C" } },
-    .{ .metric_type = Cvss31MetricType.C,   .value = "C",   .required = true,  .weights = &.{0, 0.22, 0.56},          .constraints = &.{ "N", "L", "H" } },
-    .{ .metric_type = Cvss31MetricType.I,   .value = "I",   .required = true,  .weights = &.{0, 0.22, 0.56},          .constraints = &.{ "N", "L", "H" } },
-    .{ .metric_type = Cvss31MetricType.A,   .value = "A",   .required = true,  .weights = &.{0, 0.22, 0.56},          .constraints = &.{ "N", "L", "H" } },
-    .{ .metric_type = Cvss31MetricType.E,   .value = "E",   .required = false, .weights = &.{1, 0.91, 0.94, 0.97, 1}, .constraints = &.{ "X", "U", "P", "F", "H" } },
-    .{ .metric_type = Cvss31MetricType.RL,  .value = "RL",  .required = false, .weights = &.{1, 0.95, 0.96, 0.97, 1}, .constraints = &.{ "X", "O", "T", "W", "U", "P", "C", "H" } },
-    .{ .metric_type = Cvss31MetricType.RC,  .value = "RC",  .required = false, .weights = &.{1, 0.92, 0.96, 1},       .constraints = &.{ "X", "U", "R", "C", "H" } },
-    .{ .metric_type = Cvss31MetricType.CR,  .value = "CR",  .required = false, .weights = &.{1, 0.5, 1, 1.5},         .constraints = &.{ "X", "L", "M", "H" } },
-    .{ .metric_type = Cvss31MetricType.IR,  .value = "IR",  .required = false, .weights = &.{1, 0.5, 1, 1.5},         .constraints = &.{ "X", "L", "M", "H" } },
-    .{ .metric_type = Cvss31MetricType.AR,  .value = "AR",  .required = false, .weights = &.{1, 0.5, 1, 1.5},         .constraints = &.{ "X", "L", "" } },
+    .{ .metric_type = Cvss31MetricType.AV, .required = true,  .weights = &.{0.85, 0.62, 0.55, 0.2}},
+    .{ .metric_type = Cvss31MetricType.AC, .required = true,  .weights = &.{0.44, 0.77}},
+    .{ .metric_type = Cvss31MetricType.PR, .required = true,  .weights = &.{0.85, 0.62, 0.27,
+                                                                            0.85, 0.68, 0.5}},// TODO &.{0, 0, 0}
+    .{ .metric_type = Cvss31MetricType.UI, .required = true,  .weights = &.{0.85, 0.62}},
+    .{ .metric_type = Cvss31MetricType.S,  .required = true,  .weights = &.{6.42, 7.52}},
+    .{ .metric_type = Cvss31MetricType.C,  .required = true,  .weights = &.{0, 0.22, 0.56}},
+    .{ .metric_type = Cvss31MetricType.I,  .required = true,  .weights = &.{0, 0.22, 0.56}},
+    .{ .metric_type = Cvss31MetricType.A,  .required = true,  .weights = &.{0, 0.22, 0.56}},
+    .{ .metric_type = Cvss31MetricType.E,  .required = false, .weights = &.{1, 0.91, 0.94, 0.97, 1}},
+    .{ .metric_type = Cvss31MetricType.RL, .required = false, .weights = &.{1, 0.95, 0.96, 0.97, 1}},
+    .{ .metric_type = Cvss31MetricType.RC, .required = false, .weights = &.{1, 0.92, 0.96, 1}},
+    .{ .metric_type = Cvss31MetricType.CR, .required = false, .weights = &.{1, 0.5, 1, 1.5}},
+    .{ .metric_type = Cvss31MetricType.IR, .required = false, .weights = &.{1, 0.5, 1, 1.5}},
+    .{ .metric_type = Cvss31MetricType.AR, .required = false, .weights = &.{1, 0.5, 1, 1.5}},
 
-    .{ .metric_type = Cvss31MetricType.MAV, .value = "MAV", .required = false, .weights = &.{0, 0.85, 0.62, 0.55, 0.2},   .constraints = &.{ "X", "N", "A", "L", "P" } },
-    .{ .metric_type = Cvss31MetricType.MAC, .value = "MAC", .required = false, .weights = &.{0, 0.44, 0.77},              .constraints = &.{ "X", "H", "L" } },
-    .{ .metric_type = Cvss31MetricType.MPR, .value = "MPR", .required = false, .weights = &.{0.85, 0.62, 0.27,
-                                                                                             0.85, 0.68, 0.5},            .constraints = &.{ "X", "N", "L", "H" } }, // TODO &.{0, 0, 0}
-    .{ .metric_type = Cvss31MetricType.MUI, .value = "MUI", .required = false, .weights = &.{0, 0.85, 0.62},              .constraints = &.{ "X", "N", "R" } },
-    .{ .metric_type = Cvss31MetricType.MS,  .value = "MS",  .required = false, .weights = &.{0, 6.42, 7.52},              .constraints = &.{ "X", "U", "C" } },
-    .{ .metric_type = Cvss31MetricType.MC,  .value = "MC",  .required = false, .weights = &.{0, 0, 0.22, 0.56},           .constraints = &.{ "X", "N", "L", "H" } },
-    .{ .metric_type = Cvss31MetricType.MI,  .value = "MI",  .required = false, .weights = &.{0, 0, 0.22, 0.56},           .constraints = &.{ "X", "N", "L", "H" } },
-    .{ .metric_type = Cvss31MetricType.MA,  .value = "MA",  .required = false, .weights = &.{0, 0, 0.22, 0.56},           .constraints = &.{ "X", "N", "L", "H" } },
+    .{ .metric_type = Cvss31MetricType.MAV, .required = false, .weights = &.{0, 0.85, 0.62, 0.55, 0.2}},
+    .{ .metric_type = Cvss31MetricType.MAC, .required = false, .weights = &.{0, 0.44, 0.77}},
+    .{ .metric_type = Cvss31MetricType.MPR, .required = false, .weights = &.{0.85, 0.62, 0.27,
+                                                                             0.85, 0.68, 0.5}}, // TODO &.{0, 0, 0}
+    .{ .metric_type = Cvss31MetricType.MUI, .required = false, .weights = &.{0, 0.85, 0.62}},
+    .{ .metric_type = Cvss31MetricType.MS,  .required = false, .weights = &.{0, 6.42, 7.52}},
+    .{ .metric_type = Cvss31MetricType.MC,  .required = false, .weights = &.{0, 0, 0.22, 0.56}},
+    .{ .metric_type = Cvss31MetricType.MI,  .required = false, .weights = &.{0, 0, 0.22, 0.56}},
+    .{ .metric_type = Cvss31MetricType.MA,  .required = false, .weights = &.{0, 0, 0.22, 0.56}},
 };
 // zig fmt: on
+
+fn stringToEnum(enum_type: type, str: []const u8) ?enum_type {
+    inline for (@typeInfo(enum_type).Enum.fields) |field| {
+        if (std.mem.eql(u8, field.name, str)) {
+            return @field(enum_type, field.name);
+        }
+    }
+    return null;
+}
+
+fn createMetricValue(metric_type_str: []const u8, value_str: []const u8) ?Cvss31MetricTypeValue {
+    inline for (@typeInfo(Cvss31MetricTypeValue).Union.fields) |field| {
+        if (std.mem.eql(u8, field.name, metric_type_str)) {
+            const FieldType = field.type;
+            const enum_value = stringToEnum(FieldType, value_str) orelse return null;
+            return @unionInit(Cvss31MetricTypeValue, field.name, enum_value);
+        }
+    }
+    return null;
+}
 
 pub fn score(cvss: []const u8) !types.CVSS {
     const metrics = try parseCvss31Metrics(cvss);
@@ -144,7 +195,8 @@ fn parseCvss31Metrics(cvss: []const u8) ![]Cvss31Metric {
 
         var metric_type: ?Cvss31MetricType = null;
         for (copy_cvss31_def) |*decl| {
-            if (std.mem.eql(u8, metric_type_raw.?, decl.value)) {
+            const metric_name = @tagName(decl.metric_type);
+            if (std.mem.eql(u8, metric_type_raw.?, metric_name)) {
                 if (decl.is_read == true) {
                     return types.CvssParseError.DuplicateMetric;
                 }
@@ -153,18 +205,10 @@ fn parseCvss31Metrics(cvss: []const u8) ![]Cvss31Metric {
 
                 const metric_value_raw = it_metric.rest();
 
-                var metric_value: ?[]const u8 = null;
-                for (0..(decl.constraints.len)) |j| {
-                    if (std.mem.eql(u8, metric_value_raw, decl.constraints[j])) {
-                        metric_value = decl.constraints[j];
-                        try metrics.append(.{ .metric_type = metric_type.?, .value = metric_value.?, .weight = decl.weights[j] });
-                        break;
-                    }
-                }
-                if (metric_value == null) {
-                    // std.debug.print("metric raw: {s}\n", .{metricValueRaw});
-                    // std.debug.print("metric type: {}\n", .{metricType.?});
-                    // std.debug.print("token: {s}\n", .{pair});
+                if (createMetricValue(metric_type_raw.?, metric_value_raw)) |metric_value| {
+                    try metrics.append(.{ .test_metric_type = if (@import("builtin").mode == .Debug) metric_type.?, .value = metric_value, .test_value = if (@import("builtin").mode == .Debug) metric_value_raw });
+                } else {
+                    std.log.debug("Unexpected value {any}", .{metric_value_raw});
                     return types.CvssParseError.UnknownMetricValue;
                 }
 
@@ -185,6 +229,14 @@ fn parseCvss31Metrics(cvss: []const u8) ![]Cvss31Metric {
     return metrics.items; // TODO what happens to the slice when the allocator frees the memory?
 }
 
+fn getEnumValueString(metric_value: Cvss31MetricTypeValue) []const u8 {
+    const TagType = @TypeOf(metric_value);
+    const active_tag = @as(TagType, metric_value);
+    const field_name = @tagName(active_tag);
+    // const FieldType = @TypeOf(@field(metric_value, field_name));
+    return @tagName(@field(metric_value, field_name));
+}
+
 test "parse duplicate metric CVSS:3.1/AV:N/AC:L/PR:L/UI:N/S:U/C:H/I:H/A:H/AV:N" {
     const cvss = "AV:N/AC:L/PR:L/UI:N/S:U/C:H/I:H/A:H/AV:N";
     const err = parseCvss31Metrics(cvss);
@@ -200,23 +252,24 @@ test "parse missing metricCVSS:3.1/AV:N/AC:L/PR:L/UI:N/S:U/C:H/I:H" {
 test "parse green test CVSS:3.1/AV:N/AC:L/PR:L/UI:N/S:U/C:H/I:H/A:H" {
     const cvss = "AV:N/AC:L/PR:L/UI:N/S:U/C:H/I:H/A:H";
     const metrics = try parseCvss31Metrics(cvss);
+
     try testing.expectEqual(8, metrics.len);
-    try testing.expectEqual(Cvss31MetricType.AV, metrics[0].metric_type);
-    try testing.expectEqual("N", metrics[0].value);
-    try testing.expectEqual(Cvss31MetricType.AC, metrics[1].metric_type);
-    try testing.expectEqual("L", metrics[1].value);
-    try testing.expectEqual(Cvss31MetricType.PR, metrics[2].metric_type);
-    try testing.expectEqual("L", metrics[2].value);
-    try testing.expectEqual(Cvss31MetricType.UI, metrics[3].metric_type);
-    try testing.expectEqual("N", metrics[3].value);
-    try testing.expectEqual(Cvss31MetricType.S, metrics[4].metric_type);
-    try testing.expectEqual("U", metrics[4].value);
-    try testing.expectEqual(Cvss31MetricType.C, metrics[5].metric_type);
-    try testing.expectEqual("H", metrics[5].value);
-    try testing.expectEqual(Cvss31MetricType.I, metrics[6].metric_type);
-    try testing.expectEqual("H", metrics[6].value);
-    try testing.expectEqual(Cvss31MetricType.A, metrics[7].metric_type);
-    try testing.expectEqual("H", metrics[7].value);
+    try testing.expectEqual(Cvss31MetricType.AV, metrics[0].test_metric_type);
+    try testing.expectEqualSlices(u8, "N", metrics[0].test_value);
+    try testing.expectEqual(Cvss31MetricType.AC, metrics[1].test_metric_type);
+    try testing.expectEqualSlices(u8, "L", metrics[1].test_value);
+    try testing.expectEqual(Cvss31MetricType.PR, metrics[2].test_metric_type);
+    try testing.expectEqualSlices(u8, "L", metrics[2].test_value);
+    try testing.expectEqual(Cvss31MetricType.UI, metrics[3].test_metric_type);
+    try testing.expectEqualSlices(u8, "N", metrics[3].test_value);
+    try testing.expectEqual(Cvss31MetricType.S, metrics[4].test_metric_type);
+    try testing.expectEqualSlices(u8, "U", metrics[4].test_value);
+    try testing.expectEqual(Cvss31MetricType.C, metrics[5].test_metric_type);
+    try testing.expectEqualSlices(u8, "H", metrics[5].test_value);
+    try testing.expectEqual(Cvss31MetricType.I, metrics[6].test_metric_type);
+    try testing.expectEqualSlices(u8, "H", metrics[6].test_value);
+    try testing.expectEqual(Cvss31MetricType.A, metrics[7].test_metric_type);
+    try testing.expectEqualSlices(u8, "H", metrics[7].test_value);
 }
 
 test "parse simple AV metric unknown value" {
